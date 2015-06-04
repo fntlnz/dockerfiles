ef644050029bed5bb9cd4d95fe97c865123d7561
Lorenzo Fontana <fontanalorenzo@me.com>
2015-06-04 12:30:21 +0200

    Fix image

--- scripts/flatten.sh
+++ scripts/flatten.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-image_name=$1
+image=$1
 
-container_id=$(docker run -d --entrypoint=whoami ${image_name})
+container_id=$(docker run -d --entrypoint=ls ${image})
 
-docker export ${container_id} | docker import - pippo
+docker export ${container_id} | docker import - ${image}
 
