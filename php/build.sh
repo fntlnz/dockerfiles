ad3305e8a60d7b8bfea409b0df336e18299a63a3
Lorenzo Fontana <fontanalorenzo@me.com>
2015-07-28 20:33:43 +0200

    Initial php dockerfiles refactoring

--- php/build.sh
+++ php/build.sh
@@ -1,17 +1,19 @@
-# PHP
-# @version 7.0.0beta2
-# @author Lorenzo Fontana <fontanalorenzo@me.com>
+#!/usr/bin/env bash
 
-FROM centos:centos7
+tmp_path=/tmp/dockerfiles-tmp
+image_name=
+image_versions="5.4.43 5.5.16 5.5.27 5.6.6 5.6.7 5.6.8 5.6.9 5.6.10 5.6.11 7.0.0alpha1 7.0.0alpha2 7.0.0beta1 7.0.0beta2"
 
-MAINTAINER Lorenzo Fontana, fontanalorenzo@me.com
+read -r -d '' dockerfile_template << EOM
+FROM centos:centos7\n
+MAINTAINER Lorenzo Fontana, fontanalorenzo@me.com\n
 
-WORKDIR /tmp
+WORKDIR /tmp\n
 
 RUN yum update -y \
     && yum install gcc gcc-c++ kernel-devel libxml2-devel postgresql-devel openssl-devel libcurl-devel libicu-devel readline-devel bzip2-devel tar wget make autoconf re2c bison -y \
-    && wget -nv -O - https://github.com/php/php-src/archive/php-7.0.0beta2.tar.gz | tar zx \
-    && cd /tmp/php-src-php-7.0.0beta2 \
+    && wget -nv -O - https://github.com/php/php-src/archive/php-VERSION.tar.gz | tar zx \
+    && cd /tmp/php-src-php-VERSION \
     && rm -f configure \
     && ./buildconf --force \
     && ./configure --enable-static --prefix=/usr/local/php --with-config-file-path=/usr/local/php/etc/ \
@@ -21,11 +23,22 @@ RUN yum update -y \
     --enable-sockets --enable-fpm --enable-intl \
     && make \
     && make install \
-    && rm -Rf /tmp/php-src-php-7.0.0beta2 \
+    && rm -Rf /tmp/php-src-php-VERSION \
     && ln -s /usr/local/php/bin/* /usr/local/bin \
     && ln -s /usr/local/php/sbin/* /usr/local/sbin \
     && yum remove kerne-devel libxml2-devel postgresql-devel openssl-devel libcurl-devel libicu-devel readline-devel bzip2-devel -y \
-    && yum clean all
+    && yum clean all\n
 
-WORKDIR /
-VOLUME ["/usr/local/php/etc"]
+WORKDIR /\n
+VOLUME ["/usr/local/php/etc"]\n
+EOM
+
+mkdir -p $tmp_path
+
+for version in $image_versions
+do
+    dockerfile_path=${tmp_path}/${image_name}-${version}
+    echo -e $dockerfile_template | m4 -D VERSION=$version > $dockerfile_path
+    docker build -t fntlnz/${image_name}:${version} -f $dockerfile_path $tmp_path
+    rm $dockerfile_path
+done
