8af69793fe26aa0f467f84de8e1c97700cbaff6b
Lorenzo Fontana <fontanalorenzo@me.com>
2015-08-09 22:13:13 +0200

    PHP 5.5.28

--- php/build.sh
+++ php/build.sh
@@ -3,7 +3,7 @@
 template_path=$(dirname "$0")/Dockerfile
 tmp_path=/tmp/dockerfiles-tmp
 image_name=php
-available_versions="5.4.43 5.5.16 5.5.27 5.6.6 5.6.7 5.6.8 5.6.9 5.6.10 5.6.11 5.6.12 7.0.0alpha1 7.0.0alpha2 7.0.0beta1 7.0.0beta2 7.0.0beta3"
+available_versions="5.4.43 5.5.16 5.5.27 5.5.28 5.6.6 5.6.7 5.6.8 5.6.9 5.6.10 5.6.11 5.6.12 7.0.0alpha1 7.0.0alpha2 7.0.0beta1 7.0.0beta2 7.0.0beta3"
 
 if [ -z "$1" ]; then
     echo "Usage: "
