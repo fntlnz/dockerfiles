f3f7316827e3e6e9a022d9576c8a91d17afb9aed
Lorenzo Fontana <fontanalorenzo@me.com>
2015-08-24 07:28:23 +0200

    Latest version

--- php/build.sh
+++ php/build.sh
@@ -5,7 +5,7 @@ template_path=$(dirname "$0")/Dockerfile
 tmp_path=/tmp/dockerfiles-php-tmp
 vendor_name=fntlnz
 image_name=php
-available_versions="5.4.43 5.4.44 5.5.16 5.5.27 5.5.28 5.6.6 5.6.7 5.6.8 5.6.9 5.6.10 5.6.11 5.6.12 5.6.13RC1 7.0.0alpha1 7.0.0alpha2 7.0.0beta1 7.0.0beta2 7.0.0beta3 7.0.0RC1"
+available_versions="5.4.43 5.4.44 5.5.16 5.5.27 5.5.28 5.6.6 5.6.7 5.6.8 5.6.9 5.6.10 5.6.11 5.6.13RC1 7.0.0alpha1 7.0.0alpha2 7.0.0beta1 7.0.0beta2 7.0.0beta3 7.0.0RC1 5.6.12"
 
 if [ -z "$1" ]; then
   echo "Usage: "
