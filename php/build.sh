3ef6ece081e89f31aef2a91d8430a48bd4e519eb
Lorenzo Fontana <fontanalorenzo@me.com>
2015-09-19 17:41:37 +0200

    Does not need a temporary file anymore

--- php/build.sh
+++ php/build.sh
@@ -2,7 +2,6 @@
 source "$(dirname "$0")/../utils/utils.sh"
 
 template_path=$(dirname "$0")/Dockerfile
-tmp_path=/tmp/dockerfiles-php-tmp
 vendor_name=fntlnz
 image_name=php
 available_versions="5.4.43 5.4.44 5.4.45 5.5.16 5.5.27 5.5.28 5.5.29 5.6.6 5.6.7 5.6.8 5.6.9 5.6.10 5.6.11 5.6.12 5.6.13RC1 5.6.14RC1 7.0.0alpha1 7.0.0alpha2 7.0.0beta1 7.0.0beta2 7.0.0beta3 7.0.0RC1 7.0.0RC2 7.0.0RC3 5.6.13"
@@ -17,9 +16,6 @@ version=$1
 
 check_version $version "${available_versions}"
 
-mkdir -p $tmp_path
-dockerfile_path=${tmp_path}/${image_name}-${version}
-
 configure_command="./configure --enable-static --prefix=/usr/local/php --with-config-file-path=/usr/local/php/etc/ \
 --disable-cgi --without-pear --with-sqlite3 --with-zlib --with-curl=/usr/bin/curl \
 --with-openssl --libdir=/usr/lib64 --with-libdir=lib64 --enable-opcache --enable-fileinfo --enable-mbstring \
@@ -30,16 +26,12 @@ if [[ $version == 7* ]]; then
     configure_command="${configure_command} --enable-opcache-file"
 fi
 
-cat $template_path | m4 -D VERSION=$version | m4 -D CONFIGURE_COMMAND="$configure_command" > $dockerfile_path
-
-docker build -t ${vendor_name}/${image_name}:${version} -f $dockerfile_path $tmp_path
+docker build -t ${vendor_name}/${image_name}:${version} - < <(cat $template_path | m4 -D VERSION=$version | m4 -D CONFIGURE_COMMAND="$configure_command")
 
 latest_version=$(latest_version "${available_versions}")
 if [ "$version" = "$latest_version" ]; then
   docker rmi -f ${vendor_name}/${image_name}:latest
   docker tag ${vendor_name}/${image_name}:${version} ${vendor_name}/${image_name}:latest
 fi;
-
-rm $dockerfile_path
 
 # vim:set et sw=2
